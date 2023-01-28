34bfe498044aec6c3296a62e784f49579e2d2de4
siddeshas07 <58839558+siddeshas07@users.noreply.github.com>
2023-01-28 23:53:28 +0530

    Initial commit

---
+++ movies/src/main/java/dev/siddesh/movies/ReviewRepository.java
@@ -0,0 +1,15 @@
+package dev.siddesh.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review , ObjectId> {
+
+
+
+
+
+}
